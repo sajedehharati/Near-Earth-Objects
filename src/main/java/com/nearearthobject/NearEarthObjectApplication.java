c8ed5eccf71f54caaae62ed88b4f95c1371e910c
ezharsa <sajedeh.harati.nik@ericsson.com>
2017-09-08 15:15:41 -0400

    Initial commit for project

---
+++ src/main/java/com/nearearthobject/NearEarthObjectApplication.java
@@ -0,0 +1,12 @@
+package com.nearearthobject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearEarthObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearEarthObjectApplication.class, args);
+	}
+}
